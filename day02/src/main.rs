14634c1f6703aebe03b2d77b6c955b1ac21a2270
Redmar Kerkhoff <redmar@creativecode.nl>
2020-12-02 10:00:25 +0100

    day02 part 2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -12,6 +12,15 @@ impl PasswordWithPolicy<'_> {
         let letter_count = self.password.matches(self.letter).count();
         self.range.contains(&letter_count)
     }
+
+    pub fn toboggan_password_is_valid(&self) -> bool {
+        // saving into a Range wasn't the best choice for this policy :-)
+        let pos1 = self.range.start - 1; // Toboggan Corporate Policies have no concept of "index zero"!
+        let pos2 = self.range.end - 2; // we added one and have to cope for the index starting at 1
+        let pass: Vec<char> = self.password.chars().collect();
+        let letter: char = self.letter.chars().next().unwrap();
+        (pass[pos1] == letter) ^ (pass[pos2] == letter)
+    }
 }
 
 // tried to this without the regex crate
@@ -52,6 +61,17 @@ fn main() {
         .collect();
 
     println!("#valid_passwords = {:?}", valid_passwords.len());
+
+    let valid_toboggan_passwords: Vec<PasswordWithPolicy> = input
+        .lines()
+        .map(|line| line_to_password_with_policy(line))
+        .filter(|pwp| pwp.toboggan_password_is_valid())
+        .collect();
+
+    println!(
+        "#valid_toboggan_passwords = {:?}",
+        valid_toboggan_passwords.len()
+    );
 }
 
 #[cfg(test)]
@@ -76,5 +96,17 @@ mod tests {
         let pwp = line_to_password_with_policy(&input);
 
         assert!(pwp.password_is_valid());
+    }
+
+    #[test]
+    fn test_toboggan_password_policy() {
+        let p1 = line_to_password_with_policy("1-3 a: abcde");
+        assert_eq!(p1.toboggan_password_is_valid(), true);
+
+        let p2 = line_to_password_with_policy("1-3 b: cdefg");
+        assert_eq!(p2.toboggan_password_is_valid(), false);
+
+        let p3 = line_to_password_with_policy("2-9 c: ccccccccc");
+        assert_eq!(p3.toboggan_password_is_valid(), false);
     }
 }
